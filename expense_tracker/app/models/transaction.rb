89d6ae920b31a4a32d90ca2ab4808e53acb849db
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-14 12:16:30 -0430

    fix t_type

--- expense_tracker/app/models/transaction.rb
+++ expense_tracker/app/models/transaction.rb
@@ -1,5 +1,7 @@
 class Transaction < ActiveRecord::Base
 
+	TYPE = { "ingreso" => 1, "deposito" => 2}
+
 	validates :date, presence: true
 	validates :t_type, presence: true, length: {maximum: 1}, :numericality => { :only_integer => true, :greater_than => 0 } 
 	validates :amount, presence: true, length: {maximum: 11}, :numericality => {:greater_than => 0 }
@@ -10,6 +12,11 @@ class Transaction < ActiveRecord::Base
 	def self.fechas (fecha_ini, fecha_fin)
 		Transaction.where("date>=? and date <=?", fecha_ini, fecha_fin)  	
 	end
+
+	def self.type (var)
+		var = TYPE[var] 
+		var
+	end	
 
 	# def self.category (fecha_ini, fecha_fin)
 	# 	Transaction.where("category_id=?",category_id)  	
