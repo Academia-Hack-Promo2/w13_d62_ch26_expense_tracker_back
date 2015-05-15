94f7a8f8c3058776df4b938c995d1d59e7563ff1
Jonathan Martinez <jamm@jmartinezm.org>
2015-05-14 22:50:48 -0430

    Populado de bases de datos con el archivo seeds.rb

--- expense_tracker/db/seeds.rb
+++ expense_tracker/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+Category.create(category_title: 'Casa')
+Category.create(category_title: 'Compras')
+Category.create(category_title: 'Trabajo')
+Category.create(category_title: 'Bachaqueo')
+Category.create(category_title: 'Escorts')
+Category.create(category_title: 'Otros')
+
+Transaction.create(date:'2015-05-10', t_type: 1, amount: 10000, description: 'Escapada al Euro', category_id: 1)
+Transaction.create(date:'2015-04-11', t_type: 1, amount: 3000, description: 'Gimnasio', category_id: 1)
+Transaction.create(date:'2015-04-20', t_type: 1, amount: 60000, description: 'Los roques viaje', category_id: 2)
+Transaction.create(date:'2015-05-14', t_type: 1, amount: 8000, description: 'Helado en Versailles', category_id: 2)
+Transaction.create(date:'2015-05-02', t_type: 1, amount: 500, description: 'Almuerzo en los chinos', category_id: 3)
+Transaction.create(date:'2015-04-07', t_type: 1, amount: 100, description: '', category_id: 3)
+Transaction.create(date:'2015-05-03', t_type: 1, amount: 450, description: 'Escapada al Euro', category_id: 4)
+Transaction.create(date:'2015-04-25', t_type: 1, amount: 145, description: 'Escapada al Euro', category_id: 5)
+Transaction.create(date:'2015-05-09', t_type: 1, amount: 7000, description: 'Escapada al Euro', category_id: 5)
+Transaction.create(date:'2015-05-09', t_type: 1, amount: 100, description: 'Escapada al Euro', category_id: 6)
